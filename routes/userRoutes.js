961972dc916444387bb450008cb4d584bc6e56d4
Harrison Reich <hreich1998@gmail.com>
2022-01-20 16:14:49 -0800

    get one user

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -50,4 +50,10 @@ router.get('/users', passport.authenticate('jwt'), async function (req, res) {
   res.json(users)
 })
 
+// get one user
+router.get('/user/:id', passport.authenticate('jwt'), async function (req, res) {
+  const user = await User.findById(req.params.id).populate('posts')
+  res.json(user)
+})
+
 module.exports = router
