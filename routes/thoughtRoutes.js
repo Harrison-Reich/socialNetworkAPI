9e53859d4de119c5809451a856e277ffbfa0ceb3
Harrison Reich <hreich1998@gmail.com>
2022-01-20 16:03:50 -0800

    create a thought

--- routes/thoughtRoutes.js
+++ routes/thoughtRoutes.js
@@ -3,4 +3,18 @@ const { Thought, User } = require('../models')
 const passport = require('passport')
 const { sendStatus } = require('express/lib/response')
 
+// create a thought
+router.post('/thoughts', passport.authenticate('jwt'), async function (req, res) {
+  const thought = await Thought.create({ ...req.body, user: req.user._id })
+  await User.findByIdAndUpdate(req.user._id, { $push: { thoughts: thought._id } })
+  res.json(thought)
+})
+
+// create a thought
+router.post('/thoughts', passport.authenticate('jwt'), async function (req, res) {
+  const thought = await Thought.create({ ...req.body, user: req.user._id })
+  await User.findByIdAndUpdate(req.user._id, { $push: { thoughts: thought._id } })
+  res.json(thought)
+})
+
 module.exports = router
