5469ed48ef829ca1121c106417a16eb2cf0ee4a9
Harrison Reich <hreich1998@gmail.com>
2022-01-20 16:01:31 -0800

    post one reaction

--- routes/reactionRoutes.js
+++ routes/reactionRoutes.js
@@ -1,3 +1,11 @@
 const router = require('express').Router()
 const { Thought, Reaction, User } = require('../models')
 const passport = require('passport')
+
+// post one reaction
+router.post('/reactions', passport.authenticate('jwt'), async function (req, res) {
+  const reaction = await Reaction.create({ ...req.body, user: req.user._id, thought: req.body.thoughtid })
+  await Thought.findByIdAndUpdate(req.body.thoughtid, { $push: { reactions: reaction._id } })
+  await User.findByIdAndUpdate(req.user._id, { $push: { reactions: reaction._id } })
+  res.json(reaction)
+})
