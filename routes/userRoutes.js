ea0d95c648beecd4820f39f53fac85924aa30eee
Harrison Reich <hreich1998@gmail.com>
2022-01-20 16:08:23 -0800

    make a new user

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,4 +3,13 @@ const { User, Thought } = require('../models')
 const passport = require('passport')
 const jwt = require('jsonwebtoken')
 
+// make a new user
+router.post('/users/register', (req, res) => {
+  const { username, email } = req.body
+  User.register(new User({ username, email }), req.body.password, err => {
+    if (err) { console.log(err) }
+    res.sendStatus(200)
+  })
+})
+
 module.exports = router
